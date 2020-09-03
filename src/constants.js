b0b045c2b986e1e8ab93bf4f53b1c9d6d076b9e1
Yue Yu <64169902+yuejwyu@users.noreply.github.com>
2020-09-02 20:04:25 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const SAT_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
 
-export const SAT_API_KEY = "XT36U4-NRDS3W-AL8NFP-4IC3";
+export const SAT_API_KEY = "***";
 
 export const STARLINK_CATEGORY = "52";
 
